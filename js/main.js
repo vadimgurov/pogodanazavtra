74df8d2eb072b7bd7872207af03921f02993584c
Vadim Gurov <vadim.gurov@jetbrains.com>
2014-05-04 22:55:41 +0400

    hm

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+$(function() {
+  // set date
+  var t = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+  $('#date').text(t.toLocaleDateString());
 
+  // set location
+  $('#location').text('St.-Petersburg');
+});
