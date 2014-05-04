03075160c427acf4e064dc88309fe9de43139fee
Vadim Gurov <vadim.gurov@jetbrains.com>
2014-05-05 00:45:12 +0400

    simpleWeather

--- js/main.js
+++ js/main.js
@@ -6,41 +6,21 @@ $(function() {
   // set location
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      getCity(position.coords.latitude, position.coords.longitude, function(city) {
-        $('#location').text(city);
+      getWeather(position.coords.latitude + ',' + position.coords.longitude, function(weather) {
+        $('#location').text(weather.city);
+        $('#day').html(weather.temp + '&deg;')
       });
     });
   }
-
 });
 
-function getCity(lat, lng, callback) {
-  var geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(lat, lng);
-  geocoder.geocode({'latLng': latlng}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      console.log(results);
-      if (results[1]) {
-        //find country name
-        for (var i=0; i<results[0].address_components.length; i++) {
-          for (var b=0;b<results[0].address_components[i].types.length;b++) {
-
-            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
-            if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
-              //this is the object you are looking for
-              city= results[0].address_components[i];
-              break;
-            }
-          }
-        }
-        //city data
-        console.log(city.long_name);
-        callback(city.long_name);
-      } else {
-        console.log("No results found");
-      }
-    } else {
-      console.log("Geocoder failed due to: " + status);
+function getWeather(location, callback) {
+  $.simpleWeather({
+    location: location,
+    unit: 'c',
+    success: callback,
+    error: function(error) {
+      console.log(error);
     }
   });
 }
